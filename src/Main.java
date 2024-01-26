2486da9eec6958b83e6638a2bfab78e37e0ba3bc
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-01-25 20:59:22 -0500

    Agrego de funciones

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
     static JFrame login1 = new JFrame("Iniciar Sesión");
     public static void main(String[] args) {
-        login1.setContentPane(new login1().login);
+        login1.setContentPane(new form1().pag1);
         login1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         login1.setVisible(true);
         login1.setSize(300,300);
